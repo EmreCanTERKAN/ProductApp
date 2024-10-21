a47a74720bb1ece982965514b896720779012d37
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-21 13:50:09 +0300

    Add project files.

---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            var product = new List<Product>()
+            {
+                new Product{ Id = 1 , ProductName = "Computer"},
+                new Product{ Id = 2 , ProductName = "Keyboard"},
+                new Product{ Id = 3 , ProductName = "Mouse"},
+
+            };
+            _logger.LogInformation("GettAllProduct Action has been called"); 
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct([FromBody]Product product)
+        {
+            _logger.LogWarning("Product has been created.");
+            return StatusCode(201);
+        }
+    }
+}
